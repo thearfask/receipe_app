ae0856dc74c4a7cda6c3944f8ef36aedfe3cdf9b
Arfaa <thearfask>
2019-05-18 15:38:24 +0530

    New changes to application

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,12 @@
 
 
-def  add(x, y):
+def add(x, y):
     """ Adding two numbers """
 
     return x + y
+
+
+def substract(x, y):
+    """ Substract two numbers """
+
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, substract
+
 
 class CalcTest(TestCase):
 
-
     def test_add_numbers(self):
         """ Test 2 numbs are added together """
-        self.assertEqual(add(3, 8),11)
+        self.assertEqual(add(3, 8), 11)
+
+    def test_substract_numbers(self):
+        """ Test 2 numbs are substracted successfully """
+        self.assertEqual(substract(5, 11), 6)
